a1f6f60f37fffed010f16d9d875cbe31a783a0db
Richard Solomon <35183675+RichieSolo@users.noreply.github.com>
2024-01-23 12:01:51 -0600

    change test

--- snake.py
+++
@@ -1,8 +0,0 @@
-class Snake:
-    def __init__(self):
-        self.size = 20
-        self.x = 640/2
-        self.y = 640/2
-        self.vel_x = 0
-        self.vel_y = 0
-        self.movement_speed = 400
